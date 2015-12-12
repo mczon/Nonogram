2a01f7ce1ea3e06a9e68a30860878e8bdc6adea1
Marten Carlzon <marten_czon@hotmail.com>
2015-12-12 13:42:57 +0100

    Start files for project

---
+++ src/mkc/Nonogram.java
@@ -0,0 +1,19 @@
+package mkc;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Nonogram  extends Application {
+
+    public static void main(String[] args) {
+        System.setProperty("glass.accessible.force", "false");
+        launch(args);
+    }
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
