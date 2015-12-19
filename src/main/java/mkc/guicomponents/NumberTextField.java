aa12ab5fa7f69737212aadb28094b7f6672a9798
Marten Carlzon <marten_czon@hotmail.com>
2015-12-19 07:04:10 +0100

    Tab to spaces

--- src/main/java/mkc/guicomponents/NumberTextField.java
+++ src/main/java/mkc/guicomponents/NumberTextField.java
@@ -4,63 +4,61 @@ import javafx.scene.control.TextField;
 
 public class NumberTextField extends TextField {
 
-	private boolean mAllowNegative = true;
-	private String mPositive = "[0-9]*";
-	private String mNegative = "-?[0-9]*";
+    private boolean mAllowNegative = true;
+    private final String mPositive = "[0-9]*";
+    private final String mNegative = "-?[0-9]*";
 
-	public NumberTextField(String startValue, boolean allowNegative) {
-		this(startValue);
-		mAllowNegative = allowNegative;
-	}
+    public NumberTextField(String startValue, boolean allowNegative) {
+        this(startValue);
+        mAllowNegative = allowNegative;
+    }
 
-	public NumberTextField(String startValue) {
-		super();
-		if (!"".equals(startValue)) {
-			Integer.parseInt(startValue);
-		}
-		setText(startValue);
-	}
+    public NumberTextField(String startValue) {
+        super();
+        if (!"".equals(startValue) && validate(startValue)) {
+            setText(startValue);
+        }
+    }
 
-	public NumberTextField(int startValue, boolean allowNegative) {
-		this(startValue);
-		mAllowNegative = allowNegative;
-	}
-	public NumberTextField(int startValue) {
-		super();
-		setValue(startValue);
-	}
+    public NumberTextField(int startValue, boolean allowNegative) {
+        this(startValue);
+        mAllowNegative = allowNegative;
+    }
+    public NumberTextField(int startValue) {
+        super();
+        setValue(startValue);
+    }
 
-	public void setValue(int value) {
-		setText(Integer.toString(value));
-	}
+    public void setValue(int value) {
+        super.setText(Integer.toString(value));
+    }
 
-	@Override
-	public void replaceText(int start, int end, String text) {
-		if (validate(text)) {
-			super.replaceText(start, end, text);
-		}
-	}
+    @Override
+    public void replaceText(int start, int end, String text) {
+        if (validate(text)) {
+            super.replaceText(start, end, text);
+        }
+    }
 
-	@Override
-	public void replaceSelection(String text) {
-		if (validate(text)) {
-			super.replaceSelection(text);
-		}
-	}
+    @Override
+    public void replaceSelection(String text) {
+        if (validate(text)) {
+            super.replaceSelection(text);
+        }
+    }
 
-	public int getValue() {
-		int retValue = 0;
-		if (!"".equals(getText())) {
-			retValue = Integer.parseInt(getText());
-		}
+    public int getValue() {
+        int retValue = 0;
+        if (!"".equals(getText())) {
+            retValue = Integer.parseInt(getText());
+        }
+        return retValue;
+    }
 
-		return retValue;
-	}
-
-	private boolean validate(String text) {
-		String nextValue = getText() + text;
-		return ("".equals(nextValue) ||
-				nextValue.matches(mAllowNegative ? mNegative : mPositive));
-	}
+    private boolean validate(String text) {
+        final String nextValue = getText() + text;
+        return ("".equals(nextValue) ||
+                nextValue.matches(mAllowNegative ? mNegative : mPositive));
+    }
 }
 
